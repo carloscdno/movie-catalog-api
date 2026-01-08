065219b885f3ba126cc28c5f64b21a21851d2753
Carlos C <elcordata@gmail.com>
2026-01-08 18:43:42 -0300

    Removed duplicate endpoint for movie creation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from config import settings
-from models import MovieCreate
+
 import movies
 
 # Creamos la instancia de la aplicación FastAPI
@@ -16,15 +16,6 @@ async def root():
     """Endpoint principal de la API."""
     return {"message": "Bienvenido al Catálogo de Películas 🎬"}
 
-# POST endpoint para crear una nueva película
-@app.post("/movies/")
-async def create_movie(payload: MovieCreate):
-    """Crea una nueva película en el catálogo."""
-    return {
-        "success": True,
-        "message": "Película recibida (aun sin guardar)",
-        "data": payload.model_dump()
-        }
 
 # Montamos rutas del router en /api/v1
 app.include_router(movies.router, prefix="/api/v1")
